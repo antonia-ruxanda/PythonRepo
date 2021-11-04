87b53f5c0869ac6a14e9b64bbb4fa668240a4b0a
antonia-ruxanda <antonia.ruxanda@yahoo.com>
2021-11-04 21:02:26 +0200

    Course2

---
+++ course2.py
@@ -0,0 +1,19 @@
+myList = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+ascList, descList = list(myList), list(myList)
+multiplyList = []
+
+ascList.sort()
+descList.sort(reverse=True)
+evenList = ascList[1::2]
+oddList = ascList[::2]
+
+for i in myList:
+    if i % 3 == 0:
+        multiplyList.append(i)
+
+print('Ascending list:', ascList)
+print('Descending list', descList)
+print('Even list', evenList)
+print('Odd list', oddList)
+print('Multiplies of 3', multiplyList)
+
