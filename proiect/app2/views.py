fbaef442874ca0205657e5a1ff8e80469a0560e6
antonia-ruxanda <antonia.ruxanda@yahoo.com>
2021-12-02 21:00:08 +0200

    updating

--- proiect/app2/views.py
+++ proiect/app2/views.py
@@ -1,17 +1,67 @@
 import datetime
 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 
 # Create your views here.
-from app2.models import Pontaj
+from django.urls import reverse
+from django.views.generic import ListView, CreateView, UpdateView
+
+from app2.forms import CompaniesForm
+from app2.models import Pontaj, Companies, UserExtend
 
 
+@login_required
 def newPontaj(request):
     Pontaj.objects.create(user_id=request.user.id, start_date=datetime.datetime.now())
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+@login_required
 def stopTimesheet(request):
     Pontaj.objects.filter(user_id=request.user.id, end_date=None).update(end_date=datetime.datetime.now())
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+class ListCompaniesView(LoginRequiredMixin, ListView):
+    model = Companies
+    template_name = 'app2/companies_index.html'
+
+
+class CreateCompaniesView(LoginRequiredMixin, CreateView):
+    model = Companies
+    form_class = CompaniesForm
+    template_name = 'app2/companies_form.html'
+
+    def get_form_kwargs(self):
+        variable_to_send = super(CreateCompaniesView, self).get_form_kwargs()
+        variable_to_send.update({'pk': None})
+        return variable_to_send
+
+    def get_success_url(self):
+        return reverse('app2:lista')
+
+
+class UpdateCompaniesView(LoginRequiredMixin, UpdateView):
+    model = Companies
+    form_class = CompaniesForm
+    template_name = 'app2/companies_form.html'
+
+    def get_form_kwargs(self):
+        variable_to_send = super(UpdateCompaniesView, self).get_form_kwargs()
+        variable_to_send.update({'pk': self.kwargs['pk']})
+        return variable_to_send
+
+    def get_success_url(self):
+        return reverse('app2:lista')
+
+
+class UpdateProfile(LoginRequiredMixin, UpdateView):
+    model = UserExtend
+    fields = '__all__'
+    template_name = 'app2/companies_form.html'
+
+    def get_success_url(self):
+        return reverse('app2:lista')
