5b6685404a8613b06f9db1f786c45dac80a66b47
antonia-ruxanda <antonia.ruxanda@yahoo.com>
2021-12-07 21:22:30 +0200

    new users

--- proiect/app2/urls.py
+++ proiect/app2/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('edit/<int:pk>/', views.UpdateCompaniesView.as_view(), name='modificare'),
     path('index/', views.ListCompaniesView.as_view(), name='lista'),
     path('profile/<int:pk>/', views.UpdateProfile.as_view(), name='update_profile'),
+    path('new_account', views.NewAccountView.as_view(), name='adauga_utilizator'),
 ]
 
