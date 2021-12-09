f7b49480a45bf7467ccf9c08f4ef5bd5ca28cab0
antonia-ruxanda <antonia.ruxanda@yahoo.com>
2021-12-09 21:02:43 +0200

    update

--- proiect/app1/urls.py
+++ proiect/app1/urls.py
@@ -7,5 +7,6 @@ app_name = 'app1'
 urlpatterns = [
     path('', views.CreateLocationView.as_view(), name='add'),
     path('<int:pk>/update/', views.UpdateLocationView.as_view(), name='modify'),
-    path('list/', views.ListLocationView.as_view(), name='listare')
+    path('list/', views.ListLocationView.as_view(), name='listare'),
+    path('delete/<int:pk>/', views.delete_location, name="remove"),
 ]
---
+++ proiect/jobs/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Job(models.Model):
+    name = models.CharField(max_length=100)
+    url = models.CharField(max_length=100)
+    description = models.TextField()
+    customer = models.ForeignKey('app2.Companies', on_delete=models.CASCADE)
+    active = models.IntegerField(default=1)
+
+# se real Create, Read, Update, Delete(CRUD) pentru modulul de jobs.
+# trebuie sa avem o validare in care sa nu existe job cu nume si customer identice
